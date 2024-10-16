5c4638f1f218473a816bdd3afa2de294285a2c37
@Mikist <mikeochango3@gmail.com>
2024-10-16 21:08:28 +0300

    use simple sequential chain to connect the prompts

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 from langchain import HuggingFaceHub
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 
 
@@ -14,7 +14,7 @@ prompt = st.text_input("Plug in your prompt here")
 llm = HuggingFaceHub(
     repo_id = "bigscience/bloom-1b7",
     # repo_id='tiiuae/falcon-7b-instruct',
-    model_kwargs = {'temperature':1e-10},
+    # model_kwargs = {'temperature':1e-10},
     
     
 )
@@ -24,9 +24,17 @@ title_tmplate = PromptTemplate(
     template = "Write me a youtube video title about {topic}"
 )
 
-title_chain = LLMChain(llm = llm, prompt = title_tmplate, verbose =True)
+script_tmplate = PromptTemplate(
+    input_variables=["title"],
+    template = "Write me a youtube video script based on this title TITLE: {title}"
+)
 
+
+title_chain = LLMChain(llm = llm, prompt = title_tmplate, verbose =True)
+script_chain = LLMChain(llm = llm, prompt = script_tmplate, verbose =True)
+
+sequential_chain = SimpleSequentialChain(chains = [title_chain, script_chain], verbose = True)
 
 if prompt:
-    response = title_chain.run(topic = prompt)
+    response = sequential_chain.run(prompt)
     st.write(response)
