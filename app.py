c30c87cf7c06de244faa76fbcce0cd2f2a5ac57d
@Mikist <mikeochango3@gmail.com>
2024-10-17 23:19:40 +0300

    introduced  the wikipedia api wrapper

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 from langchain import HuggingFaceHub
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 
 
@@ -15,9 +17,13 @@ llm = HuggingFaceHub(
     repo_id = "bigscience/bloom-1b7",
     # repo_id='tiiuae/falcon-7b-instruct',
     # model_kwargs = {'temperature':1e-10},
-    huggingfacehub_api_token = "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"
     
-)
+    
+    )
+
+
+
+
 # prompt templates
 title_tmplate = PromptTemplate(
     input_variables=["topic"],
@@ -25,18 +31,32 @@ title_tmplate = PromptTemplate(
 )
 
 script_tmplate = PromptTemplate(
-    input_variables=["title"],
-    template = "Write me a youtube video script based on this title TITLE: {title}"
+    input_variables=["title", 'wikipedia_research'],
+    template = "Write me a youtube video script based on this title TITLE: {title}  while leveraging this wikipedia research: {wikipedia_research}"
 )
 
 
-title_chain = LLMChain(llm = llm, prompt = title_tmplate, verbose =True, output_key = "title")
-script_chain = LLMChain(llm = llm, prompt = script_tmplate, verbose =True, output_key = "script")
 
-sequential_chain = SequentialChain(chains = [title_chain, script_chain], input_variables = ['topic'],
-                                   output_variables = ['title', 'script'], verbose = True)
+
+# Memory
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+
+
+
+title_chain = LLMChain(llm = llm, prompt = title_tmplate, verbose =True, output_key = "title", memory = title_memory)
+script_chain = LLMChain(llm = llm, prompt = script_tmplate, verbose =True, output_key = "script", memory = script_memory)
+
+
+# sequential_chain = SequentialChain(chains = [title_chain, script_chain], input_variables = ['topic'],
+#                                    output_variables = ['title', 'script'], verbose = True)
+
+
 
 if prompt:
     response = sequential_chain({'topic': prompt})
     st.write(response['title'])
     st.write(response['script'])
+
+    with st.expander("Message History"):
+        st.info(memory.buffer)
